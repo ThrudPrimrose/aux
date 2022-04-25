a5a99b2f6e2d685346e9624500ca2511974e38fc
ThrudPrimrose <yakup.paradox@gmail.com>
2022-04-25 16:55:29 +0200

    use right compilers

--- supermuc_linux_cluster_scripts/scripts_for_mpp2/builder_debug.sh
+++ supermuc_linux_cluster_scripts/scripts_for_mpp2/builder_debug.sh
@@ -17,8 +17,8 @@ cd build
 #no loop for joytypes here because every job type will have a different set of arguments so, copy by yourself
 
 cmake \
--DCMAKE_C_COMPILER=mpicc \
--DCMAKE_CXX_COMPILER=mpicxx \
+-DCMAKE_C_COMPILER=mpiicc \
+-DCMAKE_CXX_COMPILER=mpiicpc \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 -DENABLE_FILE_OUTPUT=OFF \
 -DBUILD_RELEASE=OFF \
@@ -47,8 +47,8 @@ mv pond ../pond-debug-static
 rm -r *
 
 cmake \
--DCMAKE_C_COMPILER=mpicc \
--DCMAKE_CXX_COMPILER=mpicxx \
+-DCMAKE_C_COMPILER=mpiicc \
+-DCMAKE_CXX_COMPILER=mpiicpc \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 -DENABLE_FILE_OUTPUT=OFF \
 -DBUILD_RELEASE=OFF \
@@ -76,8 +76,8 @@ mv pond ../pond-local-random
 
 rm -r *
 cmake \
--DCMAKE_C_COMPILER=mpicc \
--DCMAKE_CXX_COMPILER=mpicxx \
+-DCMAKE_C_COMPILER=mpiicc \
+-DCMAKE_CXX_COMPILER=mpiicpc \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 -DENABLE_FILE_OUTPUT=OFF \
 -DBUILD_RELEASE=OFF \
@@ -106,8 +106,8 @@ mv pond ../pond-local-busy
 rm -r *
 
 cmake \
--DCMAKE_C_COMPILER=mpicc \
--DCMAKE_CXX_COMPILER=mpicxx \
+-DCMAKE_C_COMPILER=mpiicc \
+-DCMAKE_CXX_COMPILER=mpiicpc \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 -DENABLE_FILE_OUTPUT=OFF \
 -DBUILD_RELEASE=OFF \
@@ -135,8 +135,8 @@ mv pond ../pond-global-random
 
 rm -r *
 cmake \
--DCMAKE_C_COMPILER=mpicc \
--DCMAKE_CXX_COMPILER=mpicxx \
+-DCMAKE_C_COMPILER=mpiicc \
+-DCMAKE_CXX_COMPILER=mpiicpc \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 -DENABLE_FILE_OUTPUT=OFF \
 -DBUILD_RELEASE=OFF \
